f04d77e9d70c3c1c8acf27a8801add9ca7d89af5
Chan Ho Yeung <hychandp@connect.ust.hk>
2024-09-26 19:21:05 +0800

    Add files via upload
    copilot edited

---
+++ foggytcp/src/foggy_function.cc
@@ -0,0 +1,184 @@
+/* Copyright (C) 2024 Hong Kong University of Science and Technology
+
+This repository is used for the Computer Networks (ELEC 3120) 
+course taught at Hong Kong University of Science and Technology. 
+
+No part of the project may be copied and/or distributed without 
+the express permission of the course staff. Everyone is prohibited 
+from releasing their forks in any public places. */
+
+#include <deque>
+#include <cstdlib>
+#include <cstring>
+#include <cstdio>
+
+#include "foggy_function.h"
+#include "foggy_backend.h"
+
+
+#define MIN(X, Y) (((X) < (Y)) ? (X) : (Y))
+#define MAX(X, Y) (((X) > (Y)) ? (X) : (Y))
+
+#define DEBUG_PRINT 1
+#define debug_printf(fmt, ...)                            \
+  do {                                                    \
+    if (DEBUG_PRINT) fprintf(stdout, fmt, ##__VA_ARGS__); \
+  } while (0)
+
+
+/**
+ * Updates the socket information to represent the newly received packet.
+ *
+ * In the current stop-and-wait implementation, this function also sends an
+ * acknowledgement for the packet.
+ *
+ * @param sock The socket used for handling packets received.
+ * @param pkt The packet data received by the socket.
+ */
+void on_recv_pkt(foggy_socket_t *sock, uint8_t *pkt) {
+  debug_printf("Received packet\n");
+  foggy_tcp_header_t *hdr = (foggy_tcp_header_t *)pkt;
+  uint8_t flags = get_flags(hdr);
+
+  switch (flags) {
+    case ACK_FLAG_MASK: {
+      uint32_t ack = get_ack(hdr);
+      printf("Receive ACK %d\n", ack);
+
+      sock->window.advertised_window = get_advertised_window(hdr);
+
+      if (after(ack, sock->window.last_ack_received)) {
+        sock->window.last_ack_received = ack;
+        // Slide the window
+        sock->window.last_byte_acked = ack;
+      }
+      break;
+    }
+
+    default: {
+      if (get_payload_len(pkt) > 0) {
+        uint32_t seq = get_seq(hdr);
+        debug_printf("Received data packet %d %d\n", seq, seq + get_payload_len(pkt));
+
+        sock->window.advertised_window = get_advertised_window(hdr);
+
+        // Check if the packet is within the expected sequence range
+        if (seq == sock->window.next_seq_expected) {
+          add_receive_window(sock, pkt);
+          process_receive_window(sock);
+          sock->window.next_seq_expected += get_payload_len(pkt);
+
+          // Send ACK
+          debug_printf("Sending ACK packet %d\n", sock->window.next_seq_expected);
+          uint8_t *ack_pkt = create_packet(
+              sock->my_port, ntohs(sock->conn.sin_port),
+              sock->window.last_byte_sent, sock->window.next_seq_expected,
+              sizeof(foggy_tcp_header_t), sizeof(foggy_tcp_header_t), ACK_FLAG_MASK,
+              MAX(MAX_NETWORK_BUFFER - (uint32_t)sock->received_len, MSS), 0,
+              NULL, NULL, 0);
+          sendto(sock->socket, ack_pkt, sizeof(foggy_tcp_header_t), 0,
+                 (struct sockaddr *)&(sock->conn), sizeof(sock->conn));
+          free(ack_pkt);
+        }
+      }
+      break;
+    }
+  }
+}
+
+/**
+ * Breaks up the data into packets and sends a single packet at a time.
+ *
+ * You should most certainly update this function in your implementation.
+ *
+ * @param sock The socket to use for sending data.
+ * @param data The data to be sent.
+ * @param buf_len The length of the data being sent.
+ */
+void send_pkts(foggy_socket_t *sock, uint8_t *data, int buf_len) {
+  uint8_t *data_offset = data;
+  transmit_send_window(sock);
+
+  if (buf_len > 0) {
+    while (buf_len != 0) {
+      uint16_t payload_len = MIN(buf_len, (int)MSS);
+
+      send_window_slot_t slot;
+      slot.is_sent = 0;
+      slot.msg = create_packet(
+          sock->my_port, ntohs(sock->conn.sin_port),
+          sock->window.last_byte_sent, sock->window.next_seq_expected,
+          sizeof(foggy_tcp_header_t), sizeof(foggy_tcp_header_t) + payload_len,
+          0, MAX(MAX_NETWORK_BUFFER - (uint32_t)sock->received_len, MSS), 0, NULL,
+          data_offset, payload_len);
+      sock->send_window.push_back(slot);
+
+      buf_len -= payload_len;
+      data_offset += payload_len;
+      sock->window.last_byte_sent += payload_len;
+    }
+  }
+  receive_send_window(sock);
+}
+
+void transmit_send_window(foggy_socket_t *sock) {
+  for (auto &slot : sock->send_window) {
+    if (!slot.is_sent) {
+      foggy_tcp_header_t *hdr = (foggy_tcp_header_t *)slot.msg;
+      debug_printf("Sending packet %d %d\n", get_seq(hdr), get_seq(hdr) + get_payload_len(slot.msg));
+      slot.is_sent = 1;
+      sendto(sock->socket, slot.msg, get_plen(hdr), 0, (struct sockaddr *)&(sock->conn), sizeof(sock->conn));
+    }
+  }
+}
+
+void receive_send_window(foggy_socket_t *sock) {
+  while (!sock->send_window.empty()) {
+    send_window_slot_t &slot = sock->send_window.front();
+    foggy_tcp_header_t *hdr = (foggy_tcp_header_t *)slot.msg;
+
+    if (!slot.is_sent || !has_been_acked(sock, get_seq(hdr))) {
+      break;
+    }
+
+    sock->send_window.pop_front();
+    free(slot.msg);
+  }
+}
+
+void add_receive_window(foggy_socket_t *sock, uint8_t *pkt) {
+  foggy_tcp_header_t *hdr = (foggy_tcp_header_t *)pkt;
+
+  // Stop-and-wait implementation
+  // Insert the packet into the first place of receive window
+  receive_window_slot_t *cur_slot = &(sock->receive_window[0]);
+  if (cur_slot->is_used == 0) {
+    cur_slot->is_used = 1;
+    cur_slot->msg = (uint8_t*) malloc(get_plen(hdr));
+    memcpy(cur_slot->msg, pkt, get_plen(hdr));
+  }
+}
+
+void process_receive_window(foggy_socket_t *sock) {
+  // Stop-and-wait implementation.
+  // Only process the first packet in the window.
+  receive_window_slot_t *cur_slot = &(sock->receive_window[0]);
+  if (cur_slot->is_used != 0) {
+    foggy_tcp_header_t *hdr = (foggy_tcp_header_t *)cur_slot->msg;
+    // Discard unexpected packet
+    if (get_seq(hdr) != sock->window.next_seq_expected) return;
+    // Update next seq number expected
+    uint16_t payload_len = get_payload_len(cur_slot->msg);
+    sock->window.next_seq_expected += payload_len;
+    // Copy to received_buf
+    sock->received_buf = (uint8_t*)
+        realloc(sock->received_buf, sock->received_len + payload_len);
+    memcpy(sock->received_buf + sock->received_len, get_payload(cur_slot->msg),
+           payload_len);
+    sock->received_len += payload_len;
+    // Free the slot
+    cur_slot->is_used = 0;
+    free(cur_slot->msg);
+    cur_slot->msg = NULL;
+  }
+}
